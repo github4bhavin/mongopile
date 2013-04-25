08041ee64a8cd6491b626179ffa351f1faef43eb
Bhavin Patel <mail4bhavin@gmail.com>
2013-04-25 16:15:40 -0400

    adding main class

---
+++ lib/mongopile.pm
@@ -0,0 +1,11 @@
+package mongopile;
+
+#--------------------------------------------------------------------#
+# Project : mongopile                                                #
+# Author  : Bhavin Patel                                             #
+# Purpose : Main class which hooks up into mojo framework            #
+#--------------------------------------------------------------------#
+
+$VERSION = 1.0;
+
+1;
