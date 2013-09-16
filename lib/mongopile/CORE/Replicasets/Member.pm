e9e57d9ecfac9b6379c6c535d98094271e9ee98b
github4bhavin <mail4bhavin@gmail.com>
2013-09-16 17:59:25 -0400

    updating notes with class info

--- lib/mongopile/CORE/Replicasets/Member.pm
+++ lib/mongopile/CORE/Replicasets/Member.pm
@@ -6,6 +6,25 @@ sub new {
  my $class = shift;
  my $self = {@_};
  bless $self, $class;
+
+  #Replicasets Members should have following properties
+  
+  $self->{ 'memberid' };
+  $self->{ 'name'     }; #hostname
+  $self->{ 'healthStatus' };
+  $self->{ 'replicasetState'}; # same as stateStr and state
+  $self->{ 'uptime' }; # server uptime
+  $self->{ 'optime' }; # oplog uptime
+  $self->{ 'lastHeartbeat' };
+  $self->{'pingms' };
+  
+  # MongoDB build info
+  
+  $self->{ 'mongodbBuild' }->{'}
+ 
+ 
  return $self;
 
 }
+
+sub 
