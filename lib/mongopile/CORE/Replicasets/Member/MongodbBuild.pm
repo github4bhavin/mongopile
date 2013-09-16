5df5ffc5c8dd17f67e198b08540f514623e8b11b
github4bhavin <mail4bhavin@gmail.com>
2013-09-16 18:52:13 -0400

    mongodb build object properites

--- lib/mongopile/CORE/Replicasets/Member/MongodbBuild.pm
+++ lib/mongopile/CORE/Replicasets/Member/MongodbBuild.pm
@@ -5,6 +5,15 @@ sub new {
   my $self = {@_};
   bless $self, $class;
   
+  $self->{ 'version'           }; # mongo version
+  $self->{ 'gitVersion'        }; # mongo git repo version
+  $self->{ 'sysInfo'           }; # uname -a
+  $self->{ 'bits'              }; # 32 or 64 bit machine
+  $self->{ 'debug'             }; # debug on or off
+  $self->{ 'maxBsonObjectSize' }; # max bson size allowed
+  $self->{ 'mongoProcessname'  }; # mongo process name
+  $self->{ 'writeBacksQueued'  }; # write back queue allowed? 
+  
   return $self;
 }
 
